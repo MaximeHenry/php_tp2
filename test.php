7a6e5d0d1f99876c931b67d257fe0145a0d51797
HENRY Maxime <h18000275@A-F001714.salsa.univ-amu.fr>
2019-09-25 14:39:20 +0200

    exercice 3

--- test.php
+++ test.php
@@ -5,3 +5,13 @@
 <?php
     phpinfo();
 ?>
+
+<form>
+    <input type="text" name="identifiant">
+    <input type="radio" name="civilite">
+    <input type="email" name="mail">
+    <input type="password" name="mdp">
+    <input type="password" name="verif_mdp">
+    <input type="text" name="telephone">
+    <select></select>
+</form>
