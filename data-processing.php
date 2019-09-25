51b97f46dc70e4a60ad59c45b1a076bc43a84762
HENRY Maxime <h18000275@A-F001714.salsa.univ-amu.fr>
2019-09-25 16:06:34 +0200

    Formulaire

--- data-processing.php
+++ data-processing.php
@@ -1,2 +1,17 @@
 <?php
-
+    $identifiant = $_POST['identifiant'];
+    $mail = $_POST['mail'];
+    $mdp = $_POST['mdp'];
+    $telephone = $_POST['telephone'];
+    if ($action == 'mailer')
+    {
+        $message = 'Voici votre identifiant: ' . $identifiant . PHP_EOL;
+        $message.= 'Votre mot de passe est: ' . $mdp . PHP_EOL;
+        $message.= 'Le numéro de téléphone que vous avez fourni est: ' . $telephone . PHP_EOL;
+        mail($mail, 'inscription', $message);
+    }
+    else
+    {
+        echo '<br/><strong>Bouton non géré !</strong><br/>';
+    }
+?>
--- test.php
+++ test.php
@@ -2,16 +2,31 @@
     include 'utils.inc.php';
 ?>
 
-<?php
-    phpinfo();
-?>
-
-<form>
-    <input type="text" name="identifiant">
-    <input type="radio" name="civilite">
-    <input type="email" name="mail">
-    <input type="password" name="mdp">
-    <input type="password" name="verif_mdp">
-    <input type="text" name="telephone">
-    <select></select>
+<?php start_page('formulaire') ?>
+<form action="data-processing.php" method="post">
+    <label>Identifiant</label>
+    <input type="text" name="identifiant"><br/>
+    <label>Sexe</label>
+    <input type="radio" name="civilite"><br/>
+    <label>Mail</label>
+    <input type="email" name="mail"><br/>
+    <label>Mot de passe</label>
+    <input type="password" name="mdp"><br/>
+    <label>Confirmer le mot de passe</label>
+    <input type="password" name="verif_mdp"><br/>
+    <label>Téléphone</label>
+    <input type="text" name="telephone"><br/>
+    <label>Pays</label>
+    <select id="pays">
+        <option value="France">France</option>
+        <option value="Allemagne">Allemagne</option>
+        <option value="Italie">Italie</option>
+        <option value="Russie">Russie</option>
+        <option value="Royaume-Uni">Royaume-Uni</option>
+        <option value="Espagne">Espagne</option>
+    </select><br/>
+    <label>J'accepte les conditions générales</label>
+    <input type="checkbox" name="conditions_generales"><br/>
+    <button class="submit" type="submit" action="mailer">Valider</button>
 </form>
+<?php end_page() ?>
