bd3dec23c3ea1dd67cb0b9d59d8fd1c82b149b3b
HENRY Maxime <h18000275@A-F002100.salsa.univ-amu.fr>
2019-10-03 11:11:58 +0200

    Fin exercice 5

---
+++ base.php
@@ -0,0 +1,24 @@
+<?php
+    $dbLink = mysqli_connect(dbHost, dbLogin, dbPass) or die('Erreur de connexion au serveur : '. mysqli_connect_error());
+    mysqli_select_db($dbLink, dbBd) or die('Erreur dans la selection de la base : ' . mysqli_error($dbLink));
+    $query = 'SELECT id, mail, date FROM user';
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur de requete<br/>';
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        echo 'Erreur : ' . $query . '<br/>';
+        exit();
+    }
+    while ($dbRow = mysqli_fetch_assoc($dbResult))
+    {
+        echo $dbRow['id'] . '<br/>';
+        echo $dbRow['pseudo'] . '<br/>';
+        echo $dbRow['password'] . '<br/>';
+        echo $dbRow['sexe'] . '<br/>';
+        echo $dbRow['mail'] . '<br/>';
+        echo $dbRow['telephone'] . '<br/>';
+        echo $dbRow['pays'] . '<br/>';
+        echo date('d.m.Y', strtotime($dbRow['date_inscription'])) . '<br/>';
+        echo '<br/><br/>';
+    }
+?>
--- data-processing.php
+++ data-processing.php
@@ -1,8 +1,10 @@
 <?php
     $identifiant = $_POST['identifiant'];
+    $sexe = $_POST['civilite'];
     $mail = $_POST['mail'];
     $mdp = $_POST['mdp'];
     $telephone = $_POST['telephone'];
+    $pays = $_POST['pays'];
     $action = $_POST['action'];
     if ($action == 'mailer')
     {
@@ -12,6 +14,20 @@
         mail($mail, 'inscription', $message);
         echo '<strong>Votre formulaire a bien été envoyé. </strong> <br/>';
         echo '<a href="http://maximehenry.alwaysdata.net/php_tp2/test.php"> Revenir au formulaire </a><br/>';
+
+        $dbLink = mysqli_connect(dbHost, dbLogin, dbPass) or die('Erreur de connexion au serveur : '. mysqli_connect_error());
+        mysqli_select_db($dbLink, dbBd) or die('Erreur dans la selection de la base : ' . mysqli_error($dbLink));
+        $today = date('Y-m-d');
+        $query = 'INSERT INTO user (id, pseudo, password, sexe, mail, telephone, pays, date_inscription) VALUES (NULL, \'' . $identifiant . '\',\'' . $mdp  .
+                    '\',\'' . $sexe . '\',\'' . $mail . '\',\'' . $telephone . '\',\'' . $pays . '\',\'' . $today . '\');';
+
+        if(!($dbResult = mysqli_query($dbLink, $query)))
+        {
+            echo 'Erreur de requete<br/>';
+            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+            echo 'Erreur : ' . $query . '<br/>';
+            exit();
+        }
     }
     else
     {
